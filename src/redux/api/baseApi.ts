2f0ac125abbb231774728a533c2fca05069fe665
muntasir010 <naeemtasir03@gmail.com>
2026-05-02 09:52:31 +0600

    feat: redux setup succeessfully

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
+    prepareHeaders: (headers) => {
+      // logic to get the token from localStorage and set it in the headers
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        headers.set('authorization', `${token}`);
+      }
+      return headers;
+    },
+  }),
+//   data-testid: 'base-api',
+  tagTypes: ['User', 'Item'],
+  endpoints: () => ({}),
+});
